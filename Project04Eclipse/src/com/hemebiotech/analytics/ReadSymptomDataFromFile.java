21481bc50f6bf3f957a8b60c72e2c06ab48a4f46
Aiyeesha <139870633+Aiyeesha@users.noreply.github.com>
2024-03-29 11:53:52 +0100

    Update ReadSymptomDataFromFile.java

--- Project04Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project04Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -25,7 +25,7 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 	 */
 
 	@Override
-	public List<String> GetSymptoms() {
+	public List<String> getSymptoms() {
 		List<String> result = new ArrayList<>();
 
 		try {
