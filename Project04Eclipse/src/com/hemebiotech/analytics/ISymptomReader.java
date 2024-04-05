81cd9bc5ebe3e519c7b48df6202f943bce720ba7
Aiyeesha <139870633+Aiyeesha@users.noreply.github.com>
2024-04-05 11:33:20 +0200

    Update ISymptomReader.java

--- Project04Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project04Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -3,16 +3,18 @@ package com.hemebiotech.analytics;
 import java.util.List;
 
 /**
- * Anything that will read symptom data from a source.
+ * Anything that will read symptom data from a source
  * The important part is, the return value from the operation, which is a list of strings,
- * that may contain many duplications.
- * The implementation doesn't need to order the list.
+ * that may contain many duplications
+ * 
+ * The implementation does not need to order the list
  * 
  */
 public interface ISymptomReader {
 	/**
-	 * If no data is available, return an empty List.
-	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable.
+	 * If no data is available, return an empty List
+	 * 
+	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
 	 */
-	List<String> getSymptoms ();
+	List<String> GetSymptoms ();
 }
