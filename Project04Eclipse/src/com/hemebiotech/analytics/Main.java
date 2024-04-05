d85aad7cfaaf6effbb948386c070fbe7603a3a91
Aiyeesha <139870633+Aiyeesha@users.noreply.github.com>
2024-04-05 11:52:13 +0200

    Update Main.java

--- Project04Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project04Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -1,40 +1,42 @@
 package com.hemebiotech.analytics;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-/**
- * Run the program
- */
+import java.io.File;
+import java.io.IOException;
+
+
 
 public class Main {
+	/**
+	 * Run the program
+	 *@param args
+	 *@throws IOException
+	 */
 
-    /**
-     * @param args
-     * @throws IOException
-     */
+	    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws IOException {
+	        /*
+	        Read symptoms.txt and return list
+	         */
+	    	ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile(new File("Project04Eclipse/Ressources/Symptoms.txt"));
+	        List<String> symptoms = readSymptomDataFromFile.getSymptoms();
 
-        /*
-        Read symptoms.txt and return list
-         */
-        ReadSymptomDataFromFile readData = new ReadSymptomDataFromFile(new File("Project02Eclipse/Ressources/Symptoms.txt"));
-        List<String> symptoms = readData.GetSymptoms();
+	        /*
+	        Sorts in alphabetical order and counts the occurrences of the symptoms in the list
+	         */
+	        AnalyticsCounter analyticsCounter = new AnalyticsCounter();
+	        Map<String, Integer> countSymptoms = analyticsCounter.countSymptoms(symptoms);
 
-        /*
-        Sorts in alphabetical order and counts the occurrences of the symptoms in the list
-         */
-        AnalyticsCounter analyticsCounter = new AnalyticsCounter();
-        Map<String, Integer> countSymptoms = analyticsCounter.CountSymptoms(symptoms);
+	        /*
+	        Print result in the results.out file
+	         */
+	        File fileOut = new File("Results.out");
+	        analyticsCounter.writeResults(countSymptoms, fileOut);
 
-        /*
-        Print result in the results.out file
-         */
-        File fileOut = new File("Results.out");
-        analyticsCounter.PrintFile(countSymptoms, fileOut);
 
-    }
+	}
 
 }
+
+
