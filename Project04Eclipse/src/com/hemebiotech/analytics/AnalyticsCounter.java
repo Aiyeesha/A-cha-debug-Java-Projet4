ac33ab1f0fd7806f931de53e5f4514c90ecc9ad1
Aiyeesha <139870633+Aiyeesha@users.noreply.github.com>
2024-04-07 17:27:29 +0200

    Update AnalyticsCounter.java

--- Project04Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project04Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -48,7 +48,7 @@ public class AnalyticsCounter {
 	/**
 	 * Write the map in an external file
 	 * @param countSymptoms list of symptoms in a natural order and count the strings occurrences
-	 * @param fileOut       creation of an external file
+	 * @param fileOut creation of an external file
 	 * @throws IOException if the file is not created correctly
 	 */
 
